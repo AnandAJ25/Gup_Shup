0ae92c24f451ceec00837a04f94d96af3d1b89e6
AnandAJ25 <choudharyshivam230@gmail.com>
2025-06-07 03:02:52 +0530

    bad gateway problem

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+app.get('/', (req, res) => {
+  res.send({ response: 'Server is up and running.' }).status(200);
+});
+
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
